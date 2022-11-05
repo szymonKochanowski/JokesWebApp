d0611b346d982a94f61146f0badd5d31a66ed81e
Szymon Kochanowski <szymon.piotr.kochanowski@gmail.com>
2022-11-05 11:47:02 +0100

    Added createdBy to logic

---
+++ Data/SqlQuery/InsertJokes.sql
@@ -0,0 +1,11 @@
+insert into Joke (JokeQuestion, JokeAnswer, CreatedBy) values ('What are two things you can never eat for breakfast?', 'Lunch and Dinner', 'cpetty0');
+insert into Joke (JokeQuestion, JokeAnswer, CreatedBy) values ('What is always coming but never arrives?', 'Tomorrow', 'ppeasey1');
+insert into Joke (JokeQuestion, JokeAnswer, CreatedBy) values ('What gets wetter the more it dries?', 'A towel', 'rcuttings2');
+insert into Joke (JokeQuestion, JokeAnswer, CreatedBy) values ('What can be broken but never held? ', 'A promise', 'gwoodburne3');
+insert into Joke (JokeQuestion, JokeAnswer, CreatedBy) values ('What word is spelled incorrectly in every single dictionary?', 'Incorrectly', 'ilisciardelli4');
+insert into Joke (JokeQuestion, JokeAnswer, CreatedBy) values ('What is it that lives if it is fed, and dies if you give it a drink?', 'Fire', 'rsleigh5');
+insert into Joke (JokeQuestion, JokeAnswer, CreatedBy) values ('What never asks a question but gets answered all the time?', 'Your cellphone', 'mskurray6');
+insert into Joke (JokeQuestion, JokeAnswer, CreatedBy) values ('What word would you use to describe a man who does not have all his fingers on one hand?', 'Normal, because people usually have half their fingers on one hand.', 'unarey7');
+insert into Joke (JokeQuestion, JokeAnswer, CreatedBy) values ('What goes up but never ever comes down?', 'Your age', 'lharmson8');
+insert into Joke (JokeQuestion, JokeAnswer, CreatedBy) values ('What can one catch that is not thrown?', 'A cold', 'ilickorish9');
+
--- Models/Joke.cs
+++ Models/Joke.cs
@@ -5,16 +5,19 @@
         public int Id { get; set; }
         public string JokeQuestion { get; set; }
         public string JokeAnswer { get; set; }
+        public string CreatedBy { get; set; }
+
 
         public Joke()
         {
         }
 
-        public Joke(int id, string jokeQuestion, string jokeAnswer)
+        public Joke(int id, string jokeQuestion, string jokeAnswer, string createdBy)
         {
             Id = id;
             JokeQuestion = jokeQuestion;
             JokeAnswer = jokeAnswer;
+            this.CreatedBy = createdBy;
         }
     }
 }
